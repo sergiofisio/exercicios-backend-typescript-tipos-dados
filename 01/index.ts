afb3809a675b779e73fdc9ce468c8b6cca289f09
Sergio Augusto Moreira Bastos Junior <89741627+sergiofisio@users.noreply.github.com>
2023-05-16 15:40:36 -0300

    exercicio 1 resolvido

---
+++ 01/index.ts
@@ -0,0 +1,38 @@
+const buscarDonoPet = (lista:{nome:string, pets:string[]}[], nomePet:string):string => {
+    let usuarioEncontrado;
+
+    for (const usuario of lista) {
+        if (usuario.pets.includes(nomePet)) {
+            usuarioEncontrado = usuario;
+        }
+    }
+
+    if (usuarioEncontrado) {
+        return`O dono(a) do(a) ${nomePet} é o(a) ${usuarioEncontrado.nome}`;
+    } else {
+        return`Que pena ${nomePet}, não encontramos seu dono(a)`;
+    }
+}
+
+console.log(buscarDonoPet([
+    {
+        nome: "João",
+        pets: ["Max"],
+    },
+    {
+        nome: "Ana",
+        pets: ["Pingo", "Lulu"],
+    },
+    {
+        nome: "Beatriz",
+        pets: ["Lessie"],
+    },
+    {
+        nome: "Carlos",
+        pets: ["Farofa", "Salsicha", "Batata"],
+    },
+    {
+        nome: "Antonio",
+        pets: ["Naninha"],
+    },
+], 'Salsicha'))
